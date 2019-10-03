dcfa987170302fed6e6e0c9ae5147cae5316d7f1
praterj5446 <47573930+praterj5446@users.noreply.github.com>
2019-10-02 22:52:31 -0400

    Update testdata.py

--- Adventure/testdata.py
+++ Adventure/testdata.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''Ideas for text adventure data structures'''
 
 kitchen = ["Kitchen", "This is the kitchen."]
@@ -11,3 +12,24 @@ for room in rooms:
     print("Description: ", room[1])
     print()
     print("Moving to next room...")
+
+#we could use dictionaries
+kitchenDescription = "This is the Kitchen"
+kitchenExits = {"east":"Living Room"}
+KitchenInfo = [kitchenDescription, kitchenExits]
+
+lrDescription = "There's a dusty sofa here."
+lrExits={"west":"Kitchen",
+         "south":"Bedroom"}
+lrInfo= [lrDescription, lrExits]
+
+BedroomDescription = "There is a creepy doll here."
+BedroomExits = {"west":"Living Room",
+                "north":"Bedroom"}
+BedroomInfo = [BedroomDescription, BedroomExits]
+
+roomDict = {
+    "Kitchen": ["This is the kitchen.", {"east":"Living Room"}],
+    "Living Room":["There's a dusty sofa here.",{"west":"Kitchen", "south":"Bedroom"}],
+    "Bedroom":"There is a creepy doll here."
+    }
