3df104520e3ae56b36937d5eb0edbaf12db1afff
praterj5446 <47573930+praterj5446@users.noreply.github.com>
2019-10-09 12:03:25 -0400

    Update M3LAB.py

--- Adventure/M3LAB.py
+++ Adventure/M3LAB.py
@@ -6,6 +6,8 @@ October 2,2019'''
 
 from room import Room
 
-kitchen = Room("Kitchen", "This is the kitchen.")
-livingRoom = Room("Living Room", "There's a dusty sofa here.")
-bedroom = Room("Bedroom", "There is a creepy doll here.")
+attic = Room("Attic", "The attic can only be reached by pulling the string that is hanging\nfrom the ceiling just outside the bedroom, once in the attic you get an eerie\nfeeling as if you are not supposed to be here.")
+kitchen = Room("Kitchen", "Once you enter the kitchen, at first glance the kitchen looks\nnormal. At a closer look you see what looks like animals and knifes litter the\ncounter and table. Each layed out in such a way\n that makes it look like they are not done with there work.")
+livingroom = Room("Living Room", "When you enter the living room you notice a dusty sofa\nlike it hasn't been used in a while. The entertainment stand is filled with books.\nUpon closer inspection of the room, other than the ceiling lights there appears to be no technology of any kind.")
+bedroom = Room("Bedroom", "Upon entering the bedroom, you see a worn out bed, with equally\nworn out sheets. There is a wooden dresser with a smashed mirror, sitting on the\ndresser is a doll, the doll is old, deffinetly not from this time \nera. The doll almost seems to be waiting, maybe even watching.")
+basement = Room("Basement" , "A set of stairs that lead under the hourse. The room is dark,\nno light reaches the basements from up on the stairs throught the door.\nYou hear water dripping, and smell the dampness in the room, the room feels\noddly cold for such a warm night outside with a eeire sense to the basement.")
