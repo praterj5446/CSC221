88d93663f51ccddc931dad3c70a22ec79d80ec99
praterj5446 <47573930+praterj5446@users.noreply.github.com>
2019-10-21 13:44:38 -0400

    Update

--- Adventure/M3LAB.py
+++ Adventure/M3LAB.py
@@ -6,19 +6,28 @@ October 2,2019'''
 
 from room import Room
 
-attic = Room("Attic", '''The attic can only be reached by pulling the string that is hanging from the
-ceiling just outside the bedroom, once in the attic you get an eerie feeling as if you are not
-supposed to be here.''')
+attic = Room("Attic", '''The attic can only be reached by pulling the string that is hanging
+from the ceiling just outside the bedroom, once in the attic you get an eerie feeling as if
+you are not supposed to be here.''')
 kitchen = Room("Kitchen", '''Once you enter the kitchen, at first glance the kitchen looks
 normal. At a closer look you see what looks like animals and knifes litter the counter and
 table. Each layed out in such a way\n that makes it look like they are not done with there
 work.''')
-livingroom = Room("Living Room", '''When you enter the living room you notice a dusty sofa like it hasn't
-been used in a while. The entertainment stand is filled with books. Upon closer inspection of the room,
-other than the ceiling lights there appears to be no technology of any kind.''')
-bedroom = Room("Bedroom", '''Upon entering the bedroom, you see a worn out bed, with equally worn out sheets.
-There is a wooden dresser with a smashed mirror, sitting on the dresser is a doll, the doll is old, deffinetly
-not from this time era. The doll almost seems to be waiting, maybe even watching.''')
-basement = Room("Basement" , '''A set of stairs that lead under the hourse. The room is dark, no light reaches
-the basements from up on the stairs throught the door. You hear water dripping, and smell the dampness in the
-room, the room feels oddly cold for such a warm night outside with a eeire sense to the basement.''')
+livingroom = Room("Living Room", '''When you enter the living room you notice a dusty sofa
+like it hasn't been used in a while. The entertainment stand is filled with books. Upon
+closer inspection of the room, other than the ceiling lights there appears to be no
+technology of any kind.''')
+bedroom = Room("Bedroom", '''Upon entering the bedroom, you see a worn out bed, with equally
+worn out sheets. There is a wooden dresser with a smashed mirror, sitting on the dresser is
+a doll, the doll is old, deffinetly not from this time era. The doll almost seems to be
+waiting, maybe even watching.''')
+basement = Room("Basement" , '''A set of stairs that lead under the hourse. The room is dark,
+no light reaches the basements from up on the stairs throught the door. You hear water
+dripping, and smell the dampness in the room, the room feels oddly cold for such a warm night
+outside with a eeire sense to the basement.''')
+bathroom = Room("Bathroom", '''Once inside the bathroom, you are consumed with the sense of
+darkness, even with the room being well lit with a few burning candles that seem to glow
+brighter than should be possible. The bathroom is fairly standard. There is a toilet and an
+old styled tub, it looks to be from the 1800s. There is a counter with a sink and above the
+sink is a shattered mirror. On the counter seems to be vials and beakers filled with what
+appears to be glowing fluids.''')
